3013f96c0a273eaf601a430e03994a24e779a3c6
svenskithesource <40274381+Svenskithesource@users.noreply.github.com>
2022-03-24 16:49:21 +0100

    Solve problmes

---
+++ 263.py
@@ -0,0 +1,15 @@
+num = 999
+
+def addDigits(num: int) -> int:
+    digital_root = 0
+    while num > 0:
+        digital_root += num % 10
+        num = num // 10
+        
+        if num == 0 and digital_root > 9:
+            num = digital_root
+            digital_root = 0
+            
+    return digital_root
+
+print(addDigits(num))
